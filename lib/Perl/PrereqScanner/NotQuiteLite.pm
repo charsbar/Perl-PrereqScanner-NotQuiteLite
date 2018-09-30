08183dfe71e846ed9542f27a28ab50909abfd12b
Kenichi Ishigaki <ishigaki@cpan.org>
2018-09-30 14:54:35 +0900

    allow ad-hoc in-file parser

--- lib/Perl/PrereqScanner/NotQuiteLite.pm
+++ lib/Perl/PrereqScanner/NotQuiteLite.pm
@@ -160,7 +160,10 @@ sub new {
   my @parsers = $class->_get_parsers($args{parsers});
   for my $parser (@parsers) {
     if (!exists $LOADED{$parser}) {
-      eval "require $parser; 1" or die "Parser Error: $@";
+      eval "require $parser; 1";
+      if (my $error = $@) {
+        $parser->can('register') or die "Parser Error: $error";
+      }
       $LOADED{$parser} = $parser->can('register') ? $parser->register(%args) : undef;
     }
     my $parser_mapping = $LOADED{$parser} or next;
