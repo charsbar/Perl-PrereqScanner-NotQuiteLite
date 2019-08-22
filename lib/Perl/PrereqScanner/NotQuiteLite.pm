4b622ef1a71e8caeaa660c7e01655366a0395c64
Kenichi Ishigaki <ishigaki@cpan.org>
2019-08-22 23:00:36 +0900

    release 0.9907

--- lib/Perl/PrereqScanner/NotQuiteLite.pm
+++ lib/Perl/PrereqScanner/NotQuiteLite.pm
@@ -6,7 +6,7 @@ use Carp;
 use Perl::PrereqScanner::NotQuiteLite::Context;
 use Perl::PrereqScanner::NotQuiteLite::Util;
 
-our $VERSION = '0.9906';
+our $VERSION = '0.9907';
 
 our @BUNDLED_PARSERS = qw/
   Aliased AnyMoose Autouse Catalyst ClassAccessor
